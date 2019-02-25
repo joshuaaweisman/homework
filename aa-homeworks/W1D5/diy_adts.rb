47c585287144018eccd98013778a41b74b9159b7
joshua.a.weisman@gmail.com <joshua.a.weisman@gmail.com>
2019-02-24 23:18:27 -0800

    Build map class

--- aa-homeworks/W1D5/diy_adts.rb
+++ aa-homeworks/W1D5/diy_adts.rb
@@ -50,5 +50,43 @@ class Queue
     def peek
         queue[0]
     end
+end
 
+
+
+# exercise 3: map ################################################
+class Map 
+    attr_accessor :map
+
+
+    def initialize
+        @map = []
+    end
+
+
+    def set(key, value)
+        if map.none? { |pair| pair[0] == key }
+            map << [key, value]
+        else
+            map.each { |pair| pair[1] = value if pair[0] == key }
+        end
+
+        map
+    end
+
+
+    def get(key)
+        map.each { |pair| return pair[1] if pair[0] == key }
+        nil
+    end
+
+
+    def delete(key)
+        map.each { |pair| map.delete(pair) if pair[0] == key }
+    end
+
+
+    def show
+        map
+    end
 end

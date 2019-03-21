fd84a8a3474e1c9390f4ceea77a42150a1253e38
joshua.a.weisman@gmail.com <joshua.a.weisman@gmail.com>
2019-03-20 21:56:22 -0700

    Add Big-O Notation homework exercises

--- aa-homeworks/W2D2/spec/dessert_spec.rb
+++ aa-homeworks/W2D2/spec/dessert_spec.rb
@@ -5,29 +5,51 @@ require 'dessert'
 Instructions: implement all of the pending specs (the `it` statements without blocks)! Be sure to look over the solutions when you're done.
 =end
 
+
 describe Dessert do
   let(:chef) { double("chef") }
+  let(:runts) { Dessert.new("Runts", 100, chef)}
 
   describe "#initialize" do
-    it "sets a type"
+    it "sets a type" do
+      expect(runts.type).to eq("Runts")
+    end
 
-    it "sets a quantity"
+    it "sets a quantity" do
+      expect(runts.quantity).to eq(100)
+    end
 
-    it "starts ingredients as an empty array"
+    it "starts ingredients as an empty array" do
+      expect(runts.ingredients).to eq([])
+    end
 
-    it "raises an argument error when given a non-integer quantity"
+    it "raises an argument error when given a non-integer quantity" do
+      expect { Dessert.new("Thin Mints", "lots", chef) }.to raise_error(ArgumentError)
+    end
   end
 
   describe "#add_ingredient" do
-    it "adds an ingredient to the ingredients array"
+    it "adds an ingredient to the ingredients array" do
+      runts.add_ingredient("limes")
+      expect(runts.ingredients).to include("limes")
+    end
   end
 
   describe "#mix!" do
-    it "shuffles the ingredient array"
+    it "shuffles the ingredient array" do
+      ingredients = ["lemons", "bananas", "oranges", "cherries"]
+      ingredients.each { |ingredient| runts.ingredients << ingredient }
+      runts.mix!
+      expect(runts.ingredients).not_to eq(ingredients)
+    end
   end
 
   describe "#eat" do
-    it "subtracts an amount from the quantity"
+
+
+    it "subtracts an amount from the quantity" do
+      
+    end
 
     it "raises an error if the amount is greater than the quantity"
   end
---
+++ aa-homeworks/W2D3/octopus_problems.rb
@@ -0,0 +1,60 @@
+# sluggish octopus ############################################
+def sluggish_octopus(arr)
+    max_length = 0
+    longest_fish = nil
+
+    arr.each_with_index do |fish1, idx1|
+        next if arr.any? {|fish2| fish2.length > fish1.length}
+        longest_fish = fish1
+    end
+    longest_fish
+end
+
+
+# dominant octopus ############################################
+def dominant_octopus(arr)
+    merge_sort(arr).last
+end
+
+def merge_sort(arr)
+    return arr if arr.length <= 1
+    mid_idx = arr.length / 2
+    pivot = arr[mid_idx]
+
+    left = merge_sort(arr[0...mid_idx])
+    right = merge_sort(arr[mid_idx..-1])
+
+    merge(left, right)
+end
+
+def merge(arr1, arr2)
+    final_arr = []
+
+    until arr1.empty? || arr2.empty?
+        if arr1[0].length <= arr2[0].length
+            final_arr << arr1.shift
+        else
+            final_arr << arr2.shift
+        end
+    end
+
+    final_arr + arr1 + arr2
+end
+
+
+# clever octopus ###############################################
+def clever_octopus(arr)
+    longest_fish = nil
+    longest_length = 0
+
+    arr.each do |fish|
+        if longest_fish == nil || fish.length > longest_length
+            longest_fish = fish
+            longest_length = fish.length
+        end
+    end
+    longest_fish
+end
+
+
+# slow dance ###################################################
